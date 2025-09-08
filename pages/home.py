d6921fddac241e1a030901420a5d5aae654fcff8
Aqualinqs Digital <aqualinqs22@gmail.com>
2025-09-08 11:40:56 +0000

    add ne file to home page

--- pages/home.py
+++ pages/home.py
@@ -3,7 +3,7 @@ from nicegui import ui
 def show_home_page():
     ui.label("Welcome to Akua's eatries")
 
-    with ui.row().style("flex-wrap:no-wrap"):
+    with ui.element().style("flex-wrap:no-wrap"):
         ui.image("https://cdn.pixabay.com/photo/2016/02/10/13/35/hotel-1191718_1280.jpg")
         ui.label("Cover photo")
 
