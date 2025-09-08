0b738a34448de178b14f002fcb928fbd243c54e3
Aqualinqs Digital <aqualinqs22@gmail.com>
2025-09-08 14:17:19 +0000

    new files added

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
-from nicegui import ui
-from pages.home import show_home_page
-from pages.signin import show_signin_page
-from pages.signup import show_signup_page
-from pages.event import show_event_page
-from pages.college import show_college_page
-from pages.choose_menu import show_choose_menu_page
-from pages.contact import show_contact_page
-from pages.not_found import show_not_found_page
-from components.navbar import show_navbar
-from components.footer import show_footer
+from nicegui import ui, app
+from sections import hero, welcome
 
-def page_layout(content_func, with_footer: bool = True):
-    """Reusable page layout with navbar, page content, and footer."""
-    def wrapper():
-        show_navbar()
-        content_func()
-        if with_footer:
-            show_footer()
-    return wrapper
+# Expose the assets folder to the nicegui server
+app.add_static_files("/assets", "assets")
 
-@ui.page("/")
-def home_page():
-    page_layout(show_home_page)()
+# link external icons to the head
+ui.add_head_html('''
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" 
+                 integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" 
+                 crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-@ui.page("/signin")
-def signin_page():
-    show_signin_page()
+''')
+ui.add_head_html('<link rel="stylesheet" href="/assets/reset.css" />')
 
-@ui.page("/signup")
-def signup_page():
-    show_signup_page()
-
-
-@ui.page("/choose_menu")
-def choose_menu_page():
-    page_layout(show_choose_menu_page)()
-
-
-@ui.page("/not_found")
-def not_found_page():
-    page_layout(show_not_found_page)()
-
-@ui.page("/contact")
-def contact_page():
-    page_layout(show_contact_page)()
+hero.render()
+welcome.render()
 
 ui.run(port=8000)
